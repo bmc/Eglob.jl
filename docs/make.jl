759496a53eccaa978e66b0211138d0d238313a96
Brian Clapper <bmc@clapper.org>
2017-09-27 21:51:05 -0400

    More gh-pages tweaks

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,6 @@ makedocs()
 
 open("build/index.md") do input
     open("index.md", "w") do output
-        println(output, "----")
-        println(output, "title: Eglob.jl, an extended Julia globber")
-        println(output, "layout: withTOC")
-        println(output, "----")
-        println()
         println(output, readstring(input))
     end
 end
